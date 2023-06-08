a3d81398a6536ec9a16fac8b1371d21a0e5fb70d
Denislav Ivanov <denislav.1999@abv.bg>
2023-06-08 16:28:53 +0300

    Setting the app and creating the screens
    Adding dependencies and creating the graphical user interface (GUI)

---
+++ app/src/main/java/com/example/movieinfoapp/ActivityDetailsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.movieinfoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityDetailsScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_screen)
+    }
+}
